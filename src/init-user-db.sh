40d520048ee94f356b2086ed081dbca7bddb6d2c
Vander Alves <vanderzago@gmail.com>
2019-12-31 18:51:19 -0300

    Carga inicial

--- src/init-user-db.sh
+++ src/init-user-db.sh
@@ -4,5 +4,4 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER algamoneyapp PASSWORD 'Alg@moneyap1';
     CREATE DATABASE algamoneyapi;
-    GRANT ALL PRIVILEGES ON DATABASE algamoneyapi TO algamoneyapp;
 EOSQL
